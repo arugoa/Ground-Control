0a9f154a76b66a44c6b8631cb8324b2b9d06e33b
ArihantJ <arihantjaingoa@gmail.com>
2024-04-15 18:48:35 -0700

    Initial commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Load the game scene
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void RestartGame() 
+    {
+        // Load up the start again, and reset variables
+        SceneManager.LoadScene("StartScene");
+
+        // Reset variables
+        int variables = 0;
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application (only works in standalone builds)
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/NameScroll.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContributorsLoop : MonoBehaviour
+{
+    public RectTransform[] contributors; // Array of UI elements containing contributors' names
+    public float scrollSpeed = 50f; // Speed at which the names scroll
+
+    void Update()
+    {
+        // Loop through each contributor
+        foreach (RectTransform contributor in contributors)
+        {
+            // Move the contributor's RectTransform
+            contributor.anchoredPosition += Vector2.right * scrollSpeed * Time.deltaTime;
+
+            // If the contributor's RectTransform has moved beyond the screen, reset its position to the left side
+            if (contributor.anchoredPosition.x > Screen.width)
+            {
+                contributor.anchoredPosition = new Vector2(-contributor.rect.width, contributor.anchoredPosition.y);
+            }
+        }
+    }
+}
